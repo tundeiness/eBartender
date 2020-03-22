a3ab2dae762f5f4778dff0e6667e430d966cd7bd
tundeiness <tundeiness@yahoo.co.uk>
2020-03-22 23:02:29 +0100

    setup umbrella Rails route for all React routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see
   # https://guides.rubyonrails.org/routing.html
   root 'landing#index'
+  match '*path', to: 'landing#index', via: :all
 end
