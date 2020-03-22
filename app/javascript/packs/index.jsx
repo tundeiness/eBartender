f11a86722eba2ae7425c35840390c8f0e10f9671
tundeiness <tundeiness@yahoo.co.uk>
2020-03-22 22:23:15 +0100

    rename boiler plate to index

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -3,24 +3,25 @@
 // of the page.
 
 import React from 'react'
-import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
+import ReactDOM from 'react-dom';
+import App from '../components/App';
+// import PropTypes from 'prop-types'
 
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
+// const Hello = props => (
+//   <div>Hello {props.name}!</div>
+// )
 
-Hello.defaultProps = {
-  name: 'David'
-}
+// Hello.defaultProps = {
+//   name: 'David'
+// }
 
-Hello.propTypes = {
-  name: PropTypes.string
-}
+// Hello.propTypes = {
+//   name: PropTypes.string
+// }
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Hello name="React" />,
+    <App />,
     document.body.appendChild(document.createElement('div')),
   )
 })
