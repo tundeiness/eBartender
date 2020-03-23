4ff9c0b9bb589b766761941265018b8b1b06d127
tundeiness <tundeiness@yahoo.co.uk>
2020-03-23 12:41:09 +0100

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,18 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see
   # https://guides.rubyonrails.org/routing.html
   # root 'landing#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :cocktails do
+        put :favourite, on: :member
+      end
+
+      resources :favourite_cocktails, only: %i[create destroy]
+      resources :favourite_dashboards, only: %i[index]
+    end
+  end
+
   root 'landing#app'
   match '*path', to: 'landing#app', via: :all
   #match '*path', to: 'landing#index', via: :all
