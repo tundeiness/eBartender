e00d4c9aa9b88603b556fcc60cd72ec7bd1a42fd
tundeiness <tundeiness@yahoo.co.uk>
2020-03-23 11:20:58 +0100

    edit devise/sessions/new.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'landing/index'
-  # get '/app', to: 'landing#app', as: 'app'
+  get '/index', to: 'landing#index', as: 'index'
   # For details on the DSL available within this file, see
   # https://guides.rubyonrails.org/routing.html
-  root 'landing#index'
-  match '*path', to: 'landing#index', via: :all
+  # root 'landing#index'
+  root 'landing#app'
+  match '*path', to: 'landing#app', via: :all
+  #match '*path', to: 'landing#index', via: :all
 end
