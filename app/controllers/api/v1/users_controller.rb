75eda8a8391f02716500dcdef94480ebe71e5d19
tundeiness <tundeiness@yahoo.co.uk>
2020-03-23 12:51:05 +0100

    setup users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,52 +2,11 @@ module Api
   module V1
     class UsersController < ApplicationController
       def index
-        @users = User.all
-        if @users
-          render json: {
-            users: @users
-          }
+        if user_signed_in?
+          render json: current_user
         else
-          render json: {
-            status: 500,
-            errors: ['no users found']
-          }
+          render json: {}, status: 401
         end
-      end
-
-      def show
-        if @user
-          render json: {
-            user: @user
-          }
-        else
-          render json: {
-            status: 500,
-            errors: ['user not found']
-          }
-        end
-      end
-
-      def create
-        @user = User.new(user_params)
-        if @user.save
-          login!
-          render json: {
-            status: :created,
-            user: @user
-          }
-        else
-          render json: {
-            status: 500,
-            errors: @user.errors.full_messages
-          }
-        end
-      end
-
-      private
-
-      def user_params
-        params.require(:user).permit(:username, :email, :password, :password_confirmation)
       end
     end
   end
