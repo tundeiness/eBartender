4ee517ee5aa1152b763ff0ed3a50f1da307566bd
tundeiness <tundeiness@yahoo.co.uk>
2020-03-23 12:27:44 +0100

    add validations for user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :favourite_cocktails
+  has_many :favourites, through: :favourite_cocktails, source: :cocktail
+
+  validates :username, presence: true, uniqueness: true, allow_blank: false, length: { minimum: 5 }
+  validates :email, presence: true, length: { minimum: 5 }
 end
