f5ca4c55601bb0e231ae6cec67a7c6bc7db07d13
tundeiness <tundeiness@yahoo.co.uk>
2020-03-23 21:26:02 +0100

    replace bootstrap link

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -6,7 +6,8 @@ import React from 'react'
 import ReactDOM from 'react-dom';
 import App from '../components/App';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../assets/stylesheets/application.css';
 // import PropTypes from 'prop-types'
 
