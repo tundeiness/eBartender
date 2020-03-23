904c8cde73585c34a23b19802bcc393b13331340
tundeiness <tundeiness@yahoo.co.uk>
2020-03-23 13:20:41 +0100

    setup favourite_dashboards

---
+++ app/controllers/api/v1/favourite_dashboards.rb
@@ -0,0 +1,17 @@
+module Api
+  module V1
+    class FavouriteDashboardsController < ApplicationController
+      def index
+        if user_signed_in?
+          render json: {message: 'Loading all your favourite Cocktails', data: current_user.favourites}, status: :ok
+        else
+          render json: {
+            status: 500,
+            errors: ['Not a single cocktail is found']
+          }
+        end
+      end
+
+    end
+  end
+end
