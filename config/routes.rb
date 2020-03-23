a3fc4d4465ed2b0e2b9486036b8198ec15a14550
tundeiness <tundeiness@yahoo.co.uk>
2020-03-23 09:49:34 +0100

    add app route to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'landing/index'
+  get '/app', to: 'landing#app', as: 'app'
   # For details on the DSL available within this file, see
   # https://guides.rubyonrails.org/routing.html
   root 'landing#index'
