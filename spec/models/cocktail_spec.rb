996aa47a3728173495a4afcf1f2fff3c538375db
tundeiness <tundeiness@yahoo.co.uk>
2020-03-23 14:35:35 +0100

    setup user cocktail testing

--- spec/models/cocktail_spec.rb
+++ spec/models/cocktail_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Cocktail, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:favourite_cocktails) }
+
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:description) }
+  it { should validate_presence_of(:ingredients) }
 end
