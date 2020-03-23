ad7dffd8870c64bf4047bc3d0eb16b4d62155356
tundeiness <tundeiness@yahoo.co.uk>
2020-03-23 12:48:14 +0100

    setup cocktails controller

---
+++ app/controllers/api/v1/cocktails_controller.rb
@@ -0,0 +1,67 @@
+module Api
+  module V1
+    class CocktailsController < ApplicationController
+      before_action :set_cocktail, only: %i[show favourite]
+
+        # GET /cocktails
+      def index
+        @cocktails = Cocktail.all
+        if @cocktails && user_signed_in?
+          render json: {status: 'SUCCESS', message: 'Loaded all Cocktails', data: @cocktails}, status: :ok
+        else
+          render json: {}, status: 401
+        end
+      end
+
+      def show
+        if cocktail && user_signed_in?
+          # render json: cocktail
+          render json: {status: 'SUCCESS', message: 'Showing Cocktail', data: cocktail}, status: :"Showing #{cocktail.name}"
+        else
+          render json: {
+            status: 500,
+            errors: ['cocktail not found']
+          }
+        end
+      end
+
+      def favourite
+        type = params[:type]
+        if type == 'favourite'
+          current_user.favourite_cocktails << @cocktail
+          render json: {
+            status: :"You favourited #{@cocktail.name}"
+          }
+
+        elsif type == 'unfavourite'
+          current_user.favourite_cocktails.delete(@cocktail)
+          render json: {
+            status: :"Unfavourited #{@cocktail.name}"
+          }
+
+        else
+          render json: {
+            status: 500,
+            errors: ['Nothing happened.']
+          }
+        end
+      end
+
+      private
+
+      def cocktail_params
+        # whitelist params
+        params.permit(:name, :description, :ingredients, :image)
+      end
+
+      def set_cocktail
+        Cocktail.find(params[:id])
+      end
+
+      def cocktail
+        @cocktail ||= Cocktail.find(params[:id])
+      end
+
+    end
+  end
+end
---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,54 @@
+module Api
+  module V1
+    class UsersController < ApplicationController
+      def index
+        @users = User.all
+        if @users
+          render json: {
+            users: @users
+          }
+        else
+          render json: {
+            status: 500,
+            errors: ['no users found']
+          }
+        end
+      end
+
+      def show
+        if @user
+          render json: {
+            user: @user
+          }
+        else
+          render json: {
+            status: 500,
+            errors: ['user not found']
+          }
+        end
+      end
+
+      def create
+        @user = User.new(user_params)
+        if @user.save
+          login!
+          render json: {
+            status: :created,
+            user: @user
+          }
+        else
+          render json: {
+            status: 500,
+            errors: @user.errors.full_messages
+          }
+        end
+      end
+
+      private
+
+      def user_params
+        params.require(:user).permit(:username, :email, :password, :password_confirmation)
+      end
+    end
+  end
+end
