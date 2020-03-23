1ea8dfb2568fb77730303a01ef0008bdcd914f55
tundeiness <tundeiness@yahoo.co.uk>
2020-03-23 12:16:32 +0100

    run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_23_094214) do
+ActiveRecord::Schema.define(version: 2020_03_23_110950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cocktails", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "description", null: false
+    t.string "ingredients", null: false
+    t.string "image", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", default: "", null: false
