073351c09cb46ba055c85b818a407adf6f8e5458
tundeiness <tundeiness@yahoo.co.uk>
2020-03-25 21:43:13 +0100

    correct naming in subsequent files

--- app/controllers/api/v1/favourites_dashboard.rb
+++ app/controllers/api/v1/favourites_dashboard.rb
@@ -1,6 +1,6 @@
 module Api
   module V1
-    class FavouriteDashboardsController < ApplicationController
+    class FavouritesDashboardController < ApplicationController
       def index
         if user_signed_in?
           render json: {message: 'Loading all your favourite Cocktails', data: current_user.favourites}, status: :ok
