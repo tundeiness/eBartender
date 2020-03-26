b7b07c34d577ef3884d9ba5a517970bef9da5181
tundeiness <tundeiness@yahoo.co.uk>
2020-03-27 00:51:06 +0100

    edit user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ApplicationRecord
   has_many :favourite_cocktails
   has_many :favourites, through: :favourite_cocktails, source: :cocktail
 
-  validates :username, presence: true, uniqueness: true, allow_blank: false, length: { minimum: 5 }
+  # validates :username, presence: true, uniqueness: true, allow_blank: false, length: { minimum: 5 }
   validates :email, presence: true, length: { minimum: 5 }
 end
