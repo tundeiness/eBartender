9bb154896f434b1782e5394884733e8f79121dad
tundeiness <tundeiness@yahoo.co.uk>
2020-03-26 14:10:29 +0100

    remove excess comments in App

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import Home from './Home';
-// import Cocktails from './Cocktails';
-// import Cocktails from './CocktailList;
 import Cocktails from './CocktailList';
 import SingleCocktail from './SingleCocktail';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Dashboard from './Dashboard';
-// import Cocktails from './CocktailList';
-// import { favouriteCocktails } from '../actions/index';
-// import SingleCocktail from './SingleCocktail';
 import Userfaves from './UserFave';
 
 class App extends React.Component {
@@ -21,7 +16,6 @@ class App extends React.Component {
             <Route exact path="/" component={Home} />
             <Route path="/dashboard" exact component={Dashboard} />
             <Route exact path="/cocktails" component={Cocktails} />
-            {/* <Route exact path="/singlecocktail" component={SingleCocktail} /> */}
             <Route path="/userfave" exact component={Userfaves} />
             <Route path="/singlecocktail/:id" exact component={SingleCocktail} />
           </Switch>
