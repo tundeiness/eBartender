931694219e8a9ee8c56d32a87ef99014ba01a59a
tundeiness <tundeiness@yahoo.co.uk>
2020-03-27 14:16:45 +0100

    edit index.js

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -6,7 +6,7 @@ import React from 'react'
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {ToastContainer, toast} from 'react-toastify';
+// import {ToastContainer, toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import '../stylesheets/index.css';
 import Display from '../containers/Display';
@@ -42,7 +42,6 @@ document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <Router>
       <Display store={store} />
-      <ToastContainer position={toast.POSITION.BOTTOM_LEFT}/>
     </Router>,
     document.body.appendChild(document.createElement('div')),
   )
