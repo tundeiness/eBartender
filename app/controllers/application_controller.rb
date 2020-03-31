c4a9f085179950420a517ed328dcdeeaa2a653f0
tundeiness <tundeiness@yahoo.co.uk>
2020-03-31 22:52:09 +0100

    add configure_permitted_parameters to solve unpermitted parameter bug when sign up and log in -Edit user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,9 @@
 class ApplicationController < ActionController::Base
-  before_filter :configure_permitted_parameters, if: :devise_controller?
 
-  protected
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:username, :email, :password, :remember_me ) }
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
+  before_action :configure_permitted_params, if: :devise_controller?
+
+  def configure_permitted_params
+    devise_parameter_sanitizer.permit(:sign_up,  keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update,  keys: [:username])
   end
 end
