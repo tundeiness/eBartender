a2796e6000a67c0ca5ea6679ed7079870bee8f71
tundeiness <tundeiness@yahoo.co.uk>
2020-04-11 22:59:18 +0100

    add form-helper - add form validatiion messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   has_many :favourite_cocktails
   has_many :favourites, through: :favourite_cocktails, source: :cocktail
 
-  validates :username, presence: true, uniqueness: true, allow_blank: false, length: { minimum: 5 }
-  validates :email, presence: true, length: { minimum: 5 }
+  validates :username, presence: true, uniqueness: true, allow_blank: false, length: { minimum: 5 }, :message => 'Please enter username'
+  validates :email, presence: true, length: { minimum: 5 }, :message => 'Please enter email'
+  validates :password, :message => 'Please enter password'
+  validates :password_confirmtion, :message => 'Please confirm your password'
 end
